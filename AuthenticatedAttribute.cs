827050d4dae8449ef933589f540da1176fccd25f
Dmitry Bezzubenkov <dbezzubenkov@dev-team.com>
2016-07-13 10:16:54 +0300

    authenticatedattribute source code

---
+++ AuthenticatedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace DevTeam.AuthenticatedAttribute
+{
+    public class AuthenticatedAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            var context = actionContext.Request.GetOwinContext();
+            if (!context.Authentication.User.Identity.IsAuthenticated)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
